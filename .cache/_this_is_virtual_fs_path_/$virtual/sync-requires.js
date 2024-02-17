6613e97c6656d263ec5cfbfc7bdb11a649d32d50
Kumail Akbari <kumailakbari@gmail.com>
2024-02-16 20:31:05 -0500

    updated dependencies

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/kumailakbari/Documents/GitHub/lessalone-landingsite/.cache/dev-404-page.js")),
+  "component---src-pages-404-tsx": preferDefault(require("/Users/kumailakbari/Documents/GitHub/lessalone-landingsite/src/pages/404.tsx"))
+}
+
